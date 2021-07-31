692dec4514e48bc9f29fffaccaac9886fadaa5bb
0rakul0 <0rakul0render@gmail.com>
2021-07-31 18:07:03 -0300

    att corpo

---
+++ src/js/motor1.js
@@ -0,0 +1,7 @@
+const toggle = document.getElementById('toggle');
+const body = document.querySelector('body');
+
+toggle.onclick = function() {
+    toggle.classList.toggle('active');
+    body.classList.toggle('active');
+}
