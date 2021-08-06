9148be5b8a5db9b348172048e507e911f544230a
0rakul0 <0rakul0render@gmail.com>
2021-08-05 21:32:25 -0300

    add a paginas de elementos

--- js/motor1.js
+++ js/motor1.js
@@ -1,7 +1,25 @@
+//constante do darkmode
 const toggle = document.getElementById('toggle');
 const body = document.querySelector('body');
 
+//constantes do botão de busca
+const icon = document.querySelector('.input-icon');
+const containerInput =document.querySelector(".containerInput");
+const clear = document.getElementById('clear');
+const procura = document.getElementById('procura');
+
+//função para darkmode
 toggle.onclick = function() {
     toggle.classList.toggle('active');
     body.classList.toggle('active');
+}
+
+//função para caixa de procura
+icon.onclick = function() {
+    containerInput.classList.toggle('active');
+}
+
+//função de limpar o imput
+clear.onclick = function() {
+    document.getElementById('procura').value = '';
 }
